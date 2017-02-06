f3a3c69fb8c081035dceb85e205d2e2daecd4ac5
Iavor <ivdekov@gmail.com>
2017-02-05 19:51:39 -0600

    Remove commented code

--- popup/popup.js
+++ popup/popup.js
@@ -1,25 +1,16 @@
-// const button = document.querySelector('.button');
-//
-// button.addEventListener('click', event => {
-// 	handleButtonClick(event);
-// });
-//
-// function handleButtonClick(event) {
-// 	const buttonText = button.textContent;
-// 	buttonText === 'Start' ? button.classList.add('yellow') : button.classList.remove('yellow')
-// 	button.textContent = buttonText === 'Start' ? 'Pause' : 'Start';
-// }
-
 const button = {
-	element: document.querySelector('.button'),
-	handleClick: function() {
-		const buttonText = this.textContent;
-		buttonText === 'Start' ? this.classList.add('yellow') : this.classList.remove('yellow')
-		this.textContent = buttonText === 'Start' ? 'Pause' : 'Start';
-	},
-	setUpEventListeners: function() {
-		this.element.addEventListener('click', this.handleClick);
-	}
+  element: document.querySelector('.button'),
+  handleClick: function () {
+    const buttonText = this.textContent;
+    buttonText === 'Start'
+      ? this.classList.add('yellow')
+      : this.classList.remove('yellow');
+    this.textContent = buttonText === 'Start' ? 'Pause' : 'Start';
+  },
+
+  setUpEventListeners: function () {
+    this.element.addEventListener('click', this.handleClick);
+  },
 };
 
 button.setUpEventListeners();
