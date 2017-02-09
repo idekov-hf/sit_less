2b62fc1095dc30f572a3838c65a2316fcf9c8cb2
ivdekov <ivdekov@gmail.com>
2017-02-09 16:55:01 -0600

    Add onclick att to div.button and start new popup.js

--- popup/popup.js
+++ popup/popup.js
@@ -1,33 +1,13 @@
 const timer = {
-  sittingInput: document.getElementById('sitting-duration'),
-  standingInput: document.getElementById('standing-duration'),
-  handleStart: function() {
-    setInterval(function() {
-        console.log(sittingInput.value);
-        this.sittingInput.value = sittingInput.value - 1;
-      },
-      1000
-    );
+  sittingTime: null,
+  standingTime: null,
+  start: function () {
+
   },
 };
 
-const button = {
-  element: document.querySelector('.button'),
-  handleClick: function() {
-    const buttonText = this.textContent;
-
-    buttonText === 'Start'
-      ? this.classList.add('yellow')
-      : this.classList.remove('yellow');
-
-    buttonText === 'Start' ? timer.handleStart() : '';
-
-    this.textContent = buttonText === 'Start' ? 'Pause' : 'Start';
-  },
-
-  setUpEventListeners: function() {
-    this.element.addEventListener('click', this.handleClick);
+const handlers = {
+  startTimer: function () {
+    timer.start();
   },
 };
-
-button.setUpEventListeners();
---
+++ popup/popup_old.js
@@ -0,0 +1,35 @@
+const timer = {
+  sittingInput: document.getElementById('sitting-duration'),
+  standingInput: document.getElementById('standing-duration'),
+  handleStart: function () {
+    setInterval(function () {
+        debugger;
+        console.log(this.sittingInput.value);
+        this.sittingInput.value = sittingInput.value - 1;
+      },
+
+      1000
+    );
+  },
+};
+
+const button = {
+  element: document.querySelector('.button'),
+  handleClick: function () {
+    const buttonText = this.textContent;
+
+    buttonText === 'Start'
+      ? this.classList.add('yellow')
+      : this.classList.remove('yellow');
+
+    buttonText === 'Start' ? timer.handleStart() : '';
+
+    this.textContent = buttonText === 'Start' ? 'Pause' : 'Start';
+  },
+
+  setUpEventListeners: function () {
+    this.element.addEventListener('click', this.handleClick);
+  },
+};
+
+button.setUpEventListeners();
