00f6e211ae91ada5cf4905f006c7a5388ed6a508
ivdekov <ivdekov@gmail.com>
2017-02-09 22:54:01 -0600

    Separate code into 3 main objects: timer, handlers and view. Remove onclick att from button div.

--- popup/popup.js
+++ popup/popup.js
@@ -1,13 +1,43 @@
 const timer = {
-  sittingTime: null,
-  standingTime: null,
-  start: function () {
+  sittingTime: document.getElementById('sitting-duration').value,
+  standingTime: document.getElementById('standing-duration').value,
+  tick: function () {
+    this.sittingTime--;
+  },
 
+  getTimes: function () {
+    return [this.sittingTime, this.standingTime];
   },
 };
 
 const handlers = {
   startTimer: function () {
-    timer.start();
+    setInterval(function () {
+      console.log('hello');
+      timer.tick();
+      view.displayTimer();
+    }, 1000);
+  },
+
+  start: function () {
+    console.log('hello');
   },
 };
+
+const view = {
+  displayTimer: function () {
+    const sittingInput = document.getElementById('sitting-duration');
+    const standingInput = document.getElementById('standing-duration');
+    [sittingInput.value, standingInput.value] = timer.getTimes();
+  },
+
+  setUpEventListeners: function () {
+    const startButton = document.querySelector('.button');
+    startButton.addEventListener('click', function () {
+      console.log('button clicked');
+      handlers.startTimer();
+    });
+  },
+};
+
+view.setUpEventListeners();
